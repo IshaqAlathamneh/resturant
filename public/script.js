95474f06dbc2b92256b7054dcc3ca82bd0d80c27
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-05-29 18:41:37 +0300

    done2

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 'use strict';
 // const { io } = require("socket.io-client");
-const socket = io('https://elephant-project.herokuapp.com/')
+const socket = io()
 let useName = '';
 // Get the modal
 // console.log(socket);
